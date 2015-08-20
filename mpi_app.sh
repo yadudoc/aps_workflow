8cad02da24922355b226e8e9886fc44c8b53119c
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2015-08-20 17:38:13 -0500

    Retesting on Beagle. Not quite clean yet.

--- mpi_app.sh
+++ mpi_app.sh
@@ -9,7 +9,8 @@ echo "ppn   : $ppn"
 echo "nodes : $nodes"
 
 cd $PBS_O_WORKDIR
-PATH=/home/users/p01532/mpich-3.1.4/install/bin:$PATH
+PATH=$MPI_INSTALL:$PATH
+
 
 hosts=$(aprun -n $nodes -d $ppn -N 1 /bin/hostname | grep nid0)
 hosts=$(echo $hosts | sed -e "s/ /,/g")
@@ -19,7 +20,7 @@ echo service hostname=$(hostname -f)
 
 # dummyhosts=$(seq -s , -f "H%03.0f" 10)
 
-/home/users/p01532/mpich-3.1.4/install/bin/mpiexec -iface=ipogif0 -launcher manual -hosts $hosts -n $nproc $MPI_APP_PATH infile outraven 1 2>&1 | tee mpiexec.out &
+$MPI_INSTALL/mpiexec -iface=ipogif0 -launcher manual -hosts $hosts -n $nproc $MPI_APP_PATH infile outraven 1 2>&1 | tee mpiexec.out &
 
 sleep 3
 echo launchlist:
--- mpi_driver.swift
+++ mpi_driver.swift
@@ -1,6 +1,6 @@
 type file;
 
-app (file out, file err) mpi_app (file mpi_sh, file data)
+app (file out, file err) mpi_app (file mpi_sh, file prox, file data)
 {
     bash @mpi_sh stdout=@out stderr=@err;
 }
@@ -15,9 +15,10 @@ file mandel_out[] <simple_mapper; prefix="output/mandel_", suffix=".out">;
 file mandel_err[] <simple_mapper; prefix="output/mandel_", suffix=".err">;
 file mpi_sh <"./mpi_app.sh">;
 file data <"./data">;
+file prox <"./prox">;
 
 foreach i in [5:itermax:step]{
     tracef("i = %i \n", i);
-    (mandel_out[i], mandel_err[i]) = mpi_app(mpi_sh, data);
+    (mandel_out[i], mandel_err[i]) = mpi_app(mpi_sh, prox, data);
 }
 
